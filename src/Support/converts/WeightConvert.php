72fa8301e32871abf08070c46318268c89f26bb7
dengminfeng <1015814408@qq.com>
2022-07-26 22:02:32 +0800

    feat: 重量转换辅助类

---
+++ src/Support/converts/WeightConvert.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Rice\Basic\Support\converts;
+
+
+class WeightConvert extends MeterBase
+{
+
+    // 公制
+    public const METRIC_T  = 't';          // 吨
+    public const METRIC_KG = 'kg';         // 千克
+    public const METRIC_G  = 'g';          // 克
+    public const METRIC_MG = 'mg';         // 毫克
+
+    // 常衡制
+    public const BRITISH_ST  = 'st';        // 英石
+    public const BRITISH_UKT = 'ukt';       // 英国长吨
+    public const BRITISH_UST = 'ust';       // 美国短吨
+    public const BRITISH_LB  = 'lb';        // 磅
+    public const BRITISH_OZ  = 'oz';        // 盎司
+
+    /**
+     * @overwrite
+     * @var string $anchorPointUnit
+     */
+    protected $anchorPointUnit = self::METRIC_KG;
+
+    protected $calculates = [
+        // 公制
+        self::METRIC_T    => '1000',
+        self::METRIC_KG => '1',
+        self::METRIC_G  => '0.001',
+        self::METRIC_MG => '0.000001',
+        // 常衡制
+        self::BRITISH_ST => '6.35029',
+        self::BRITISH_UKT => '1016.047',
+        self::BRITISH_UST => '907.1847',
+        self::BRITISH_LB  => '0.453592',
+        self::BRITISH_OZ  => '0.0283495',
+    ];
+}
---
+++ tests/Support/Converts/WeightConvertTest.php
@@ -0,0 +1,25 @@
+<?php
+
+
+namespace Tests\Support\Converts;
+
+
+use PHPUnit\Framework\TestCase;
+use Rice\Basic\Support\converts\WeightConvert;
+
+class WeightConvertTest extends TestCase
+{
+    public function testLengthConvert(): void
+    {
+        $weightConvert = new WeightConvert('1', WeightConvert::METRIC_G);
+        $this->assertEquals('0.001', $weightConvert->getNum(3));
+
+        $weightConvert = new WeightConvert('1', WeightConvert::METRIC_KG);
+        $this->assertEquals('1', $weightConvert->getNum());
+
+        $this->assertEquals('6.3', $weightConvert->to(WeightConvert::BRITISH_ST, 1));
+
+        $this->assertEquals('0.0283', $weightConvert->to(WeightConvert::BRITISH_OZ));
+
+    }
+}
