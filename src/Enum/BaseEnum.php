84fac770dd08c9f3a1de3ed51bee2be12d952b19
dengminfeng <1015814408@qq.com>
2022-12-13 21:35:02 +0800

    perf: 优化枚举类 & feat: 增加枚举类获取常量数据测试

--- src/Enum/BaseEnum.php
+++ src/Enum/BaseEnum.php
@@ -4,8 +4,44 @@ namespace Rice\Basic\Enum;
 
 abstract class BaseEnum
 {
-    public static function getConstants()
+    // 属性未定义
+    public const ATTR_NOT_DEFINE = 'attr_not_define';
+    // 属性未定义
+    public const METHOD_NOT_DEFINE = 'method_not_define';
+
+    // 全部常量
+    public static $consts = null;
+
+    // 父级常量
+    public static $parentConsts = null;
+
+    // 子级常量
+    public static $childConsts = null;
+
+    public static function getConstants(): array
     {
-        return (new \ReflectionClass(self::class))->getConstants();
+        if (is_null(self::$consts)) {
+            return self::$consts = (new \ReflectionClass(static::class))->getConstants();
+        }
+
+        return self::$consts;
+    }
+
+    public static function getParentConstants(): array
+    {
+        if (is_null(self::$parentConsts)) {
+            return self::$parentConsts = (new \ReflectionClass(self::class))->getConstants();
+        }
+
+        return self::$parentConsts;
+    }
+
+    public static function getChildConstants(): array
+    {
+        if (is_null(self::$childConsts)) {
+            return self::$childConsts = array_diff(self::getConstants(), self::getParentConstants());
+        }
+
+        return self::$childConsts;
     }
 }
---
+++ tests/Support/EnumTest.php
@@ -0,0 +1,27 @@
+<?php
+
+
+namespace Tests\Support;
+
+
+use PHPUnit\Framework\TestCase;
+use Rice\Basic\Enum\ExceptionEnum;
+
+class EnumTest extends TestCase
+{
+    public function testGetConstants(): void
+    {
+        $this->assertIsArray(ExceptionEnum::getChildConstants());
+        $arr = [ExceptionEnum::ATTR_NOT_DEFINE, ExceptionEnum::METHOD_NOT_DEFINE];
+
+        $this->assertEquals($arr, [
+            ExceptionEnum::getParentConstants()['ATTR_NOT_DEFINE'],
+            ExceptionEnum::getParentConstants()['METHOD_NOT_DEFINE'],
+        ]);
+
+        $this->assertEquals($arr, [
+            ExceptionEnum::getConstants()['ATTR_NOT_DEFINE'],
+            ExceptionEnum::getConstants()['METHOD_NOT_DEFINE'],
+        ]);
+    }
+}
