bb5113e6436a5904ac8b556230e559bfc600e2ce
dengmf <1015814408@qq.com>
2023-02-27 18:48:51 +0800

    fix: path目录修复

--- src/PathManager.php
+++ src/PathManager.php
@@ -17,6 +17,7 @@ class PathManager
     public string $cache;
     public string $src;
     public string $test;
+    public string $components;
     public string $assembler;
     public string $console;
     public string $dto;
@@ -29,18 +30,19 @@ class PathManager
 
     public function __construct()
     {
-        $this->project   = __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR;
-        $this->cache     = $this->project . 'cache' . DIRECTORY_SEPARATOR;
-        $this->src       = $this->project . 'src' . DIRECTORY_SEPARATOR;
-        $this->test      = $this->project . 'tests' . DIRECTORY_SEPARATOR;
-        $this->assembler = $this->src . 'Assembler' . DIRECTORY_SEPARATOR;
-        $this->console   = $this->src . 'Console' . DIRECTORY_SEPARATOR;
-        $this->dto       = $this->src . 'DTO' . DIRECTORY_SEPARATOR;
-        $this->entity    = $this->src . 'Entity' . DIRECTORY_SEPARATOR;
-        $this->enum      = $this->src . 'Enum' . DIRECTORY_SEPARATOR;
-        $this->exception = $this->src . 'Exception' . DIRECTORY_SEPARATOR;
-        $this->lang      = $this->src . 'Lang' . DIRECTORY_SEPARATOR;
-        $this->support   = $this->src . 'Support' . DIRECTORY_SEPARATOR;
-        $this->template  = $this->src . 'Template' . DIRECTORY_SEPARATOR;
+        $this->project    = __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR;
+        $this->cache      = $this->project . 'cache' . DIRECTORY_SEPARATOR;
+        $this->src        = $this->project . 'src' . DIRECTORY_SEPARATOR;
+        $this->test       = $this->project . 'tests' . DIRECTORY_SEPARATOR;
+        $this->components = $this->src . 'components' . DIRECTORY_SEPARATOR;
+        $this->assembler  = $this->components . 'Assembler' . DIRECTORY_SEPARATOR;
+        $this->dto        = $this->components . 'DTO' . DIRECTORY_SEPARATOR;
+        $this->entity     = $this->components . 'Entity' . DIRECTORY_SEPARATOR;
+        $this->enum       = $this->components . 'Enum' . DIRECTORY_SEPARATOR;
+        $this->exception  = $this->components . 'Exception' . DIRECTORY_SEPARATOR;
+        $this->console    = $this->src . 'Console' . DIRECTORY_SEPARATOR;
+        $this->lang       = $this->src . 'Lang' . DIRECTORY_SEPARATOR;
+        $this->support    = $this->src . 'Support' . DIRECTORY_SEPARATOR;
+        $this->template   = $this->src . 'Template' . DIRECTORY_SEPARATOR;
     }
 }
